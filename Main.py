7b333a321c2e740064a64e23928cd522d91a91fa
hskbborsa <164005291+hskbborsa@users.noreply.github.com>
2024-03-19 16:10:35 +0300

    a

--- Main.py
+++ Main.py
@@ -54,7 +54,23 @@ if __name__ == "__main__":
 
 
 
+#side bar: switcher
+gender=st.sidebar.multiselect(
+    label="Select Gender",
+    options=df["gender"].unique(),
+    default=df["gender"].unique().
+    )
 
+stream=st.sidebar.multiselect(
+    label="select Stream",
+    options=df["stream"].unique(),
+    default=df["stream"].unique(),
+    )
+comment=st.sidebar.multiselect(
+    label="select Comment",
+    options=df["comment"].unique(),
+     default=df["comment"].unique(),
+    )
 
 
 #get selected item
@@ -98,3 +114,110 @@ with st.expander("⏱ Search student by name"):
         text_search=""
 
 
+
+
+if 'df' not in st.session_state:
+    df = pd.DataFrame({
+        'Animal':['Dog','Dog','Dog','Dog','Cat','Cat','Spider','Spider'],
+        'Breed':['bulldog','X','X','asky','Y','Y','asky','asky'],
+        'Color':['Brown','Black','Black','Green','Yellow','Brown','White','Black']
+    })
+    st.session_state.df = df
+
+df = st.session_state.df
+df_filtered = df.copy()
+
+# Session state values to track the order of filter selection
+if 'confirmed' not in st.session_state:
+    st.session_state.confirmed = []
+    st.session_state.last_edited = None
+
+def last_edited(i,col):
+    '''Update session state values to track order of editing
+    
+    i:int
+        index of the column that was last edited
+    col:str
+        name of the column that was last edited
+    '''
+    if st.session_state.last_edited is None: # Nothing was previously selected/edited
+        st.session_state.last_edited = (i,col)
+        return
+    if st.session_state.last_edited == (i,col): # The same column was last edited
+        undo(col)
+        return
+    # Some other column was last edited:
+    confirmed(*st.session_state.last_edited)
+    st.session_state.last_edited = (i,col)
+    return
+        
+def undo(col):
+    '''Undoes the last confirmation if the last edit was to clear a filter
+
+    col : str
+        name of the column that was last edited
+    '''
+    if st.session_state['col_'+col] == []: # Check state of widget by key
+        last_confirmed = safe_pop(st.session_state.confirmed,-1)
+        st.session_state.last_edited = last_confirmed
+
+def safe_pop(lst, i):
+    '''Pops the ith element of a list, returning None if the index is out of bounds
+    
+    lst : list
+        list to pop from
+    i : int
+        index to pop
+    '''
+    try:
+        return lst.pop(i)
+    except IndexError:
+        return None
+
+def confirmed(i,col):
+    '''Adds the last edited column to the confirmed list
+    
+    i:int
+        index of the column that was last edited
+    col:str
+        name of the column that was last edited
+    '''
+    st.session_state.confirmed.append((i,col))
+
+# Columns to display the filters (Streamlit with create multiselect widgets
+# according to the order of user edits, but columns will keep them displaying
+# in their original order for the user)
+cols = st.columns(3)
+
+selected = dict(zip(df.columns, [[],[],[]]))
+
+# Confirmed filters
+for i,col in st.session_state.confirmed:
+    selected[col] = cols[i].multiselect(
+        col, df_filtered[col].unique(), key=f'col_{col}', 
+        on_change=last_edited, args=[i,col], disabled=True
+    )
+    df_filtered = df_filtered[df_filtered[col].isin(selected[col])]
+
+#Currently editing
+if st.session_state.last_edited is not None:
+    i,col = st.session_state.last_edited
+    selected[col] = cols[i].multiselect(
+        col, df_filtered[col].unique(), key=f'col_{col}', 
+        on_change=last_edited, args=[i,col]
+    )
+    df_filtered = df_filtered[df_filtered[col].isin(selected[col])]
+
+# Not yet edited filters
+for i, col in enumerate(df_filtered.columns):
+    if (i,col) not in st.session_state.confirmed and (i,col) != st.session_state.last_edited:
+        selected[col] = cols[i].multiselect(
+            col, df_filtered[col].unique(), key=f'col_{col}', 
+            on_change=last_edited, args=[i,col]
+        )
+    if selected[col] != []:
+        df_filtered = df_filtered[df_filtered[col].isin(selected[col])]
+
+cols = st.columns(2)
+cols[0].write(df)
+cols[1].write(df_filtered)
