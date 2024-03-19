b73f378b08809439b514838eb8b700234c4a3175
hskbborsa <164005291+hskbborsa@users.noreply.github.com>
2024-03-19 14:33:07 +0300

    csv

--- Main.py
+++ Main.py
@@ -31,6 +31,19 @@ comment=st.sidebar.multiselect(
     options=df["comment"].unique(),
      default=df["comment"].unique(),
     )
+
+uploaded_file = st.file_uploader(“Choose a file”)
+if uploaded_file is not None:
+#read csv
+df1=pd.read_csv(uploaded_file)
+
+   #read xls or xlsx
+   df1=pd.read_excel(uploaded_file)
+else:
+st.warning(“you need to upload a csv or excel file.”)
+
+
+
 #get selected item
 
 df_selection=df.query(
