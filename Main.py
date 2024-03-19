fb978af377dfccf69386c74f6e15a06254f3b6e4
hskbborsa <164005291+hskbborsa@users.noreply.github.com>
2024-03-19 14:39:58 +0300

    aa

--- Main.py
+++ Main.py
@@ -33,7 +33,7 @@ comment=st.sidebar.multiselect(
     )
 
 
-uploaded_file = st.file_uploader("Choose a file")
+uploaded_file = st.sidebar.file_uploader("Choose a file")
 if uploaded_file is not None:
    #read csv
    #df1=pd.read_csv(uploaded_file)
